8a882400ee7fb9da627c3ec10a0f448d5a9ed772
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-07-30 02:35:41 +0200

    Feat: Fetch and display weather data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
-import { getWeatherByCityname as getData } from './weather-api.js';
+import { getWeatherByCoordinates } from './weather-api.js';
 
+// Set default city to graz.
+let city = 'graz';
 
+const currentTmp = document.querySelector('.current-tmp');
+const tempMin = document.querySelector('.tmp-min');
+const tempMax = document.querySelector('.tmp-max');
 
+// Fetch and display the weather for a city.
+const fetchWeatherForCity = async cityName => {
+	try {
+		const response = await fetch('data/cities.json');
+		const data = await response.json();
+
+		const getCityData = data.cities.find(
+			element => element['city'] === cityName
+		);
+
+		const weatherData = await getWeatherByCoordinates(
+			getCityData.lat,
+			getCityData.lon
+		);
+
+		currentTmp.innerHTML = weatherData.main.temp.toFixed() + '°';
+		tempMin.innerHTML = weatherData.main.temp_min.toFixed() + '°';
+		tempMax.innerHTML = weatherData.main.temp_max.toFixed() + '°';
+	} catch (error) {
+		console.log('Failed to fetch the Weather data', error);
+	}
+};
+
+// First API Call for default city.
+fetchWeatherForCity(city);
+
+// Get selected city.
+let selectedCity = document.querySelector('.cities');
+
+// Set an event for city, if it gets update by the user then make again an API Call.
+selectedCity.addEventListener('change', event => {
+	city = event.target.value;
+	fetchWeatherForCity(city);
+});
