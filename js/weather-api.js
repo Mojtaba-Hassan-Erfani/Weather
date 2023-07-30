314d0452a763b66e5c12aa17df827af9a22a45a0
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-07-30 02:35:03 +0200

    Feat: Openweather api call implementation

--- js/weather-api.js
+++ js/weather-api.js
@@ -1,3 +1,8 @@
-export let getWeatherByCityname = ( city ) => {
-    // TODO:
+export const getWeatherByCoordinates = async ( lat, lon  ) => {
+    const apiKey = Here_will_come_your_OpenWeather_API_KEY;
+
+    const response = await fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric` );
+    const data = await response.json();
+
+    return data;
 }
